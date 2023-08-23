8e317c88c636220fb97df4b10da9bfe1e0cf5998
Your Name <you@example.com>
2023-08-23 15:33:52 +0000

    test

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/ck-vannguyen-hcp/network/google"
+  version = "7.3.0"
+  # insert required variables here
+
+
+subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
