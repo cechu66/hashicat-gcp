b407ac36d98caf3a488f714d3b0f45ddbf540909
Your Name <you@example.com>
2023-08-23 16:14:27 +0000

    test

--- vpc.tf
+++ vpc.tf
@@ -4,12 +4,11 @@ module "network" {
   version = "3.4.0"
   # insert required variables here
  project_id   = var.project
-    network_name = "example-vpc"
-    routing_mode = "GLOBAL"
+    network_name = "example-vpc11"
 
 subnets = [
   {
-    subnet_name   = "gaurav-subnet"
+    subnet_name   = "gaurav-subneiit"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
